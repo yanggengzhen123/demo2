044bd6c0505ab430ed36b388ac44c3c3d9ad724d
yanggengzhen <15017290625@163.com>
2020-12-02 23:49:52 +0800

    branch1提交

---
+++ vue练习/vue-pro/src/pages/IndexPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <Tab />
+  </div>
+</template>
+
+<script>
+import Tab from '@/components/Tab/index.vue'
+export default {
+  name: 'IndexPage',
+  components:{
+      Tab
+  },
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
