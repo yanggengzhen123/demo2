7a31173caf55c3beb507e9b068814bdf71cc136c
yanggengzhen <15017290625@163.com>
2020-12-03 23:00:36 +0800

    小修改

---
+++ vue练习/vue-pro/src/components/Tab/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        11
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+    created(){
+        this.test();
+    },
+    methods: {
+        test(){
+            var AA = function(){
+                this.focus = {
+                    name:'yang',
+                    age:18
+                }
+            }
+            AA.prototype.focus = {
+                name:'geng',
+                age:18
+            }
+            var bb = new AA()
+            var cc = new AA()
+            bb.focus.name = 'zhen'
+            cc.focus.name = 'liyixuan'
+            console.log(bb.focus.name)
+            console.log(cc.focus.name)
+        }
+    },
+};
+</script>
+
+<style>
+
+</style>
